7055ce6d33bf63b30a6d9c66ac22ce567927ba61
Charlie Paterson <Charlieed.Paterson@googlemail.com>
2023-06-13 11:44:49 +0100

    Setup: Defined a basic interface that would be desired for a stack. Create, push, pop, see inside

--- headers/dyn_structures.h
+++ headers/dyn_structures.h
@@ -1,7 +1,17 @@
 
+// Focusing on pointers to structs, for now
+
 typedef struct stack {
 	void *contents;
 	int items_held;
 	int capacity;
 } Stack;
+
+Stack new_stack (int);
+
+void push_to (Stack, void*);
+
+void* pop_from (Stack);
+
+void check_inside (Stack);
 
